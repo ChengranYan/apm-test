65c23de1a7abaa163fa6d9b1b9360bd8bdd8f729
Chengran.Y <503163002@qq.com>
2019-01-07 18:58:24 +0800

    initinit

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import * as Sentry from '@sentry/browser'
 
 Vue.config.productionTip = false
 
+Sentry.init({
+  release: '2.0',
+  dsn: 'https://55feca8d735d4f8d949359fab44c8cfb@sentry.io/1365248',
+  integrations: [new Sentry.Integrations.Vue({
+    Vue
+  })]
+})
 new Vue({
   router,
   render: h => h(App)
